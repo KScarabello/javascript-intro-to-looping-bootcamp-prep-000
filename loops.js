c6c39f7e4296eb5f1b42a00833398bb9d7904db7
Kimberly Scarabello <kimberly.scarabello@gmail.com>
2016-09-28 00:31:38 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,20 @@
 
-//var array = 25
 
-//function forLoop(array){
-//  for (var i = 1; i < array.length; i++) {
-//      console.log("I am " + i + " strange loop."}
-
-function whileLoop(n) {
-  while (n > 0){
-    console.log(--n)}
+function forLoop(array){
+  for (var i = 1; i < 25; i++) {
+     if (i === 1){
+       array.push("I am 1 strange loop.")
+     } else {
+       array.push("I am" + i + "strange loops.")
+     }
 }
+return array
+}
+
+
+
+//function whileLoop(n) {
+  //while (n > 0){
+    //console.log(--n)}
+    //return n
+//}
