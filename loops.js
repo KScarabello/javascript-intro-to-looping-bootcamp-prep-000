84bf84c3500fe6dabeb7940dd40faa3dced342c5
Kimberly Scarabello <kimberly.scarabello@gmail.com>
2016-09-28 00:01:38 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,11 @@
+
+//var array = 25
+
+//function forLoop(array){
+//  for (var i = 1; i < array.length; i++) {
+//      console.log("I am " + i + " strange loop."}
+
+function whileLoop(n) {
+  while (n > 0){
+    console.log(--n)}
+}
